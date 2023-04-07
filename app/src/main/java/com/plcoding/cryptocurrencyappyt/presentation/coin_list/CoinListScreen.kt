3c01b6901634a3a118c5d7a89832db6ed404bac1
amandubeyy <dubey7920@gmail.com>
2023-04-07 14:02:27 +0530

    route added

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/coin_list/CoinListScreen.kt
@@ -0,0 +1,14 @@
+package com.plcoding.cryptocurrencyappyt.presentation.coin_list
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+
+@Composable
+fun CoinListScreen(
+    navController: NavController,
+    coinListViewModel: CoinListViewModel = hiltViewModel()
+) {
+    val state = coinListViewModel.state.value
+
+}
